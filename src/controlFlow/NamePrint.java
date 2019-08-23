3dda055173fb284cfe25e8e7fb4bf2e1739ac380
ahsan870 <ahsanpabel@gmail.com>
2019-08-23 00:33:46 -0400

    Added name class

---
+++ src/controlFlow/NamePrint.java
@@ -0,0 +1,12 @@
+package controlFlow;
+
+public class NamePrint {
+
+	public static void main(String[] args) {
+	String firstName = "Ahsan ";
+	String lastName = "Ahmed";
+
+	System.out.println("My name is "+ firstName + lastName);
+	}
+
+}
